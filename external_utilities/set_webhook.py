331e91d091f9c6f51743339b5e5f417dd6f01f7b
Alan <alanjohnston800@gmail.com>
2023-01-07 02:16:41 +0000

    webhook script update

--- external_utilities/set_webhook.py
+++ external_utilities/set_webhook.py
@@ -2,9 +2,13 @@ import telegram
 
 print("provide token")
 token = input()
-print("provide webhook url")
+print("provide webhook url (or no to use existing)")
 url = input()
 
 bot = telegram.Bot(token=token)
 
-bot.set_webhook(url, allowed_updates=["MESSAGE"]) and print("Successfully updated")
+if url == "no":
+    url = bot.get_webhook_info()['url']
+
+
+bot.set_webhook(url, allowed_updates=["new_chat_members"]) and print("Successfully updated")
